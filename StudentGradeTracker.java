2059b255950f1a6a45f5a7208ad9b388af22d38c
Ravinga Deshani <147974756+ravinga221@users.noreply.github.com>
2025-05-23 15:16:24 +0530

    Create Student class

--- StudentGradeTracker.java
+++ StudentGradeTracker.java
@@ -1,6 +1,34 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Student 
+{
+    private String name;
+    private double grade;
+    
+    public Student(String name, double grade) 
+	{
+        this.name = name;
+        this.grade = grade;
+    }
+    
+    public String getName() 
+	{
+        return name;
+    }
+    
+    public double getGrade() 
+	{
+        return grade;
+    }
+    
+    @Override
+    public String toString() 
+	{
+        return String.format("%s: %.2f", name, grade);
+    }
+}
+
 public class StudentGradeTracker 
 {
     public static void main(String[] args) 
